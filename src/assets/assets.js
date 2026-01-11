6d526e39fd8d81fea893f2484db2a3431039edeb
SAIDU MUJAHEED ZARMAI <164806136+Mujaheed003@users.noreply.github.com>
2026-01-11 17:53:43 +0100

    Update assets.js
    image path change

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,6 +5,13 @@ import testimonial1 from "./testimonial_img_1.jpg";
 import testimonial2 from "./testimonial_img_2.jpg";
 import testimonial3 from "./testimonial_img_3.jpg";
 
+import latest1 from './latest_image_1.jpg';
+import latest2 from './latest_image_2.jpg';
+import latest3 from './latest_image_3.jpg';
+import latest4 from './latest_image_4.jpg';
+
 export const assets = { logo, BuildingImg };
 
 export const testimonialsImg = { testimonial1,testimonial2, testimonial3 };
+
+export const latestImg = { latest1, latest2, latest3, latest4 };
