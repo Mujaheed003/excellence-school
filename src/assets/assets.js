09fba6dfb1b2996e79eb13ca6ed56310bb0cb4f1
SAIDU MUJAHEED ZARMAI <164806136+Mujaheed003@users.noreply.github.com>
2026-01-11 17:30:16 +0100

    Add testimonial images to assets.js
    image path change

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,10 @@
 import logo from "./logo.png";
 import BuildingImg from "./building_future_img.jpg";
 
+import testimonial1 from "./testimonial_img_1.jpg";
+import testimonial2 from "./testimonial_img_2.jpg";
+import testimonial3 from "./testimonial_img_3.jpg";
+
 export const assets = { logo, BuildingImg };
+
+export const testimonials = { testimonial1,testimonial2, testimonial3 };
